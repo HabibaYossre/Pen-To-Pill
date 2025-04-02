f1709aa49e9e76eeba99b88a41cff5862b01c5cc
Renad <renad03emara@gmail.com>
2025-04-02 20:28:45 +0200

    Frontend Done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PrescriptionTracker from './PrescriptionTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <PrescriptionTracker />
+    </div>
+  );
+}
+
+export default App;
